ce7643303c7d7e2ae22ebd537804614971dcf9fa
mattshrew <125844743+mattshrew@users.noreply.github.com>
2023-03-03 11:55:46 -0500

    Removed .x class, default Xs and Os

--- assets/js/ttt.js
+++ assets/js/ttt.js
@@ -1,7 +1,7 @@
 var gameActive = true;
 const squareIDs = ['top-left', 'top-middle', 'top-right', 'middle-left', 'middle-middle', 'middle-right', 'bottom-left', 'bottom-middle', 'bottom-right'];
 const colours = ['#60A0FF', '#A060FF', '#fff'];
-const players = ['⨯', 'ⵔ'];
+const players = ['X', 'O'];
 var player = 0;
 var board = ['', '', '', '', '', '', '', '', ''];
 
@@ -22,7 +22,6 @@ window.addEventListener("DOMContentLoaded", function() {
 function playMove(square) {
     square.style.color = colours[player];
     square.innerHTML = players[player];
-    if (player == 0) square.classList.add('x');
     board[squareIDs.indexOf(square.id)] = players[player];
     player = (player + 1) % 2;
     document.getElementsByClassName('game__info')[0].style.color = colours[player];
@@ -81,6 +80,5 @@ function resetGame() {
     for (let i = 0; i < squareIDs.length; i++) {
         document.getElementById(squareIDs[i]).innerHTML = '';
         document.getElementById(squareIDs[i]).classList.toggle('is-active');
-        document.getElementById(squareIDs[i]).classList.remove('x');
     }
 }
