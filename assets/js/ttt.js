46375f4360f86af31c52b14a626c42f4fdb4d8c7
mattshrew <125844743+mattshrew@users.noreply.github.com>
2023-03-10 14:03:27 -0500

    Debugging ttt.js...

--- assets/js/ttt.js
+++ assets/js/ttt.js
@@ -65,10 +65,11 @@ async function computerMove() {
             empty_squares.push(i);
         }
     }
-    
+    alert(1);
     for (let i = 2; i > 0; i--) {
         for (let j = 0; i < empty_squares.length; j++) {
             board[j] = players[player];
+            alert(2);
             if (checkResult(board) == i) {
                 board[j] = '';
                 playMove(document.getElementById(squareIDs[j]));
@@ -78,7 +79,7 @@ async function computerMove() {
             }
         }
     }
-    
+    alert(3);
     move = empty_squares[Math.floor(Math.random()*empty_squares.length)];
     playMove(document.getElementById(squareIDs[move]));    
 }
