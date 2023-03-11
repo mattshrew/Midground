fe89b9dfd615466db80a80524e00a8a076e29e8f
John Doe <MidKnight8822@gmail.com>
2023-03-10 19:49:33 -0500

    Hard (impossible?) TTT computer

--- assets/js/ttt.js
+++ assets/js/ttt.js
@@ -57,27 +57,217 @@ function sleep(ms) {
 //     playMove(document.getElementById(squareIDs[move]));
 // }
 
+// async function computerMove() {
+//     await sleep(500);
+//     let empty_squares = [];
+//      for (let i = 0; i < board.length; i++) {
+//         if (board[i] == '') {
+//             empty_squares.push(i);
+//         }
+//     }
+//     for (let i = 2; i > 0; i--) {
+//         for (let j = 0; j < empty_squares.length; j++) {
+//             board[empty_squares[j]] = players[i-1];
+//             if (checkResult(board) == i) {
+//                 board[empty_squares[j]] = '';
+//                 playMove(document.getElementById(squareIDs[empty_squares[j]]));
+//                 return;
+//             } else {
+//                 board[empty_squares[j]] = '';
+//             }
+//         }
+//     }
+//     move = empty_squares[Math.floor(Math.random()*empty_squares.length)];
+//     playMove(document.getElementById(squareIDs[move]));    
+// }
+
 async function computerMove() {
     await sleep(500);
     let empty_squares = [];
+    let empty_corners = [];
+    let empty_edges = [];
+
      for (let i = 0; i < board.length; i++) {
         if (board[i] == '') {
             empty_squares.push(i);
+            if (i == 0 || i == 2 || i == 6 || i == 8) empty_corners.push(i);
+            if (i == 1 || i == 3 || i == 5 || i == 7) empty_edges.push(i);
         }
     }
+
+    let empty = empty_squares.length;
+
+    switch (empty) {
+        case 9:
+            let choices = [0, 2, 4, 6, 8];
+            playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+            return;
+        case 8:
+            if (board[4] == '') {
+                playMove(document.getElementById(squareIDs[4]));
+            } else {
+                playMove(document.getElementById(squareIDs[empty_corners[Math.floor(Math.random()*empty_corners.length)]]));
+            }
+            return;
+        case 6:
+            if (board[4] == players[0]) {
+                if ((board[0] != '' && board[8] != '') || (board[2] != '' && board[6] != '')) {
+                    playMove(document.getElementById(squareIDs[empty_corners[Math.floor(Math.random()*empty_corners.length)]]));
+                    return;
+                } else if (board[1] == players[0]) {
+                    if (board[7] == '') playMove(document.getElementById(squareIDs[7]));
+                    return;
+                } else if (board[3] == players[0]) {
+                    if (board[5] == '') playMove(document.getElementById(squareIDs[5]));
+                    return;
+                } else if (board[5] == players[0]) {
+                    if (board[3] == '') playMove(document.getElementById(squareIDs[3]));
+                    return;
+                } else if (board[7] == players[0]) {
+                    if (board[1] == '') playMove(document.getElementById(squareIDs[1]));
+                    return;
+                }
+            } else if (board[0] == players[0]) {
+                if (board[1] == players[0]) {
+                    playMove(document.getElementById(squareIDs[2]));
+                    return;
+                } else if (board[2] == players[0]) {
+                    playMove(document.getElementById(squareIDs[1]));
+                    return;
+                } else if (board[3] == players[0]) {
+                    playMove(document.getElementById(squareIDs[6]));
+                    return;
+                } else if (board[6] == players[0]) {
+                    playMove(document.getElementById(squareIDs[3]));
+                    return;
+                } else if (board[5] == players[0]) {
+                    let choices = [2, 7, 8];
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[7] == players[0]) {
+                    let choices = [6, 5, 8];
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[8] == players[0]) {
+                    playMove(document.getElementById(squareIDs[empty_edges[Math.floor(Math.random()*empty_edges.length)]]));
+                    return;
+                }
+            } else if (board[2] == players[0]) {
+                if (board[1] == players[0]) {
+                    playMove(document.getElementById(squareIDs[0]));
+                    return;
+                } else if (board[0] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[1]));
+                    return;
+                } else if (board[5] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[8]));
+                    return;
+                } else if (board[8] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[5]));
+                    return;
+                } else if (board[3] == players[0]) { 
+                    let choices = [0, 6, 7];
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[7] == players[0]) { 
+                    let choices = [3, 6, 8];
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[6] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[empty_edges[Math.floor(Math.random()*empty_edges.length)]]));
+                    return;
+                }
+            } else if (board[6] == players[0]){
+                if (board[3] == players[0]) {
+                    playMove(document.getElementById(squareIDs[0]));
+                    return;
+                } else if (board[0] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[3]));
+                    return;
+                } else if (board[7] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[8]));
+                    return;
+                } else if (board[8] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[7]));
+                    return;
+                } else if (board[5] == players[0]) { 
+                    let choices = [1, 2, 8];
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[1] == players[0]) { 
+                    let choices = [0, 2, 5];
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[2] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[empty_edges[Math.floor(Math.random()*empty_edges.length)]]));
+                    return;
+                }
+            } else if (board[8] == players[0]) {
+                if (board[5] == players[0]) {
+                    playMove(document.getElementById(squareIDs[2]));
+                    return;
+                } else if (board[2] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[5]));
+                    return;
+                } else if (board[7] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[6]));
+                    return;
+                } else if (board[6] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[7]));
+                    return;
+                } else if (board[3] == players[0]) { 
+                    let choices = [0, 1, 6]
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[1] == players[0]) { 
+                    let choices = [0, 3, 2]
+                    playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                    return;
+                } else if (board[0] == players[0]) { 
+                    playMove(document.getElementById(squareIDs[empty_edges[Math.floor(Math.random()*empty_edges.length)]]));
+                    return;
+                }
+                return;
+            } else if (board[1] == players[0] && board[3] == players[0]) {
+                let choices = [0, 2, 6];
+                playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                return;
+            } else if (board[3] == players[0] && board[7] == players[0]) {
+                let choices = [0, 6, 8];
+                playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                return;
+            } else if (board[7] == players[0] && board[5] == players[0]) {
+                let choices = [2, 6, 8];
+                playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                return;
+            } else if (board[5] == players[0] && board[1] == players[0]) {
+                let choices = [0, 2, 8];
+                playMove(document.getElementById(squareIDs[choices[Math.floor(Math.random()*choices.length)]]));
+                return;
+            } else if (board[1] == players[0] && board[7] == players[0]) {
+                playMove(document.getElementById(squareIDs[empty_corners[Math.floor(Math.random()*empty_corners.length)]]));
+                return;
+            } else if (board[3] == players[0] && board[5] == players[0]) {
+                playMove(document.getElementById(squareIDs[empty_corners[Math.floor(Math.random()*empty_corners.length)]]));
+                return;
+            }
+    }
+
+
     for (let i = 2; i > 0; i--) {
-        for (let j = 0; j < empty_squares.length; j++) {
-            board[j] = players[player];
+        for (let j = 0; j < empty; j++) {
+            board[empty_squares[j]] = players[i-1];
             if (checkResult(board) == i) {
-                board[j] = '';
-                playMove(document.getElementById(squareIDs[j]));
+                board[empty_squares[j]] = '';
+                playMove(document.getElementById(squareIDs[empty_squares[j]]));
                 return;
             } else {
-                board[j] = '';
+                board[empty_squares[j]] = '';
             }
         }
     }
-    move = empty_squares[Math.floor(Math.random()*empty_squares.length)];
+    
+    let move = empty_squares[Math.floor(Math.random()*empty)];
     playMove(document.getElementById(squareIDs[move]));    
 }
 
@@ -88,7 +278,7 @@ function checkResult(board) {
         if (board[i] == '') empty++;
     }
 
-    if (empty >= 5) return 0;
+    if (empty > 5) return 0;
 
     for (let i = 0; i < 2; i++) {
         const p = players[i];
@@ -116,6 +306,7 @@ function endGame(result) {
         document.getElementById('game__result').innerHTML = '‎Draw!';
     } else {
         document.getElementById('game__result').innerHTML = `${players[result - 1]} has won!`;
+        if (gameMode == "computer" && result == 1) document.getElementById('game__result').innerHTML = '‎Please tell me how..';
     }
     document.getElementById('game__info').style.color = colours[result - 1];
     document.getElementById('game__info').innerHTML = "‎Game Over!";
